d6e714860667c46d70f20145f174466cb69005fb
André Duarte <140335@corp.hrln.br>
2024-12-19 13:03:16 -0300

    terminando o curso

--- js/api.js
+++ js/api.js
@@ -1,10 +1,23 @@
 const URL_BASE = "http://localhost:3000"
 
+const converStringParaData = (dataString) => {
+  const [ano, mes, dia] = dataString.split('-');
+  return new Date(Date.UTC(ano, mes - 1, dia));
+  //2024-08-12 = [2024, 08, 12]
+}
+
 const api = {
   async buscarPensamentos() {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos`)
-      return await response.data
+      const pensamentos = await response.data
+
+      return pensamentos.map(pensamento => {
+        return {
+          ...pensamento,
+          data: new Date(pensamento.data)
+        }
+      });
     }
     catch {
       alert('Erro ao buscar pensamentos')
@@ -14,7 +27,11 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+      const data = converStringParaData(pensamento.data);
+      const response = await axios.post(`${URL_BASE}/pensamentos`, {
+        ...pensamento,
+        data: data.toISOString()
+      })
       return await response.data
     }
     catch {
@@ -26,7 +43,11 @@ const api = {
   async buscarPensamentoPorId(id) {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-      return await response.data
+      const pensamento = await response.data;
+      return {
+        ...pensamento,
+        data: new Date(pensamento.data)
+      };
     }
     catch {
       alert('Erro ao buscar pensamento')
@@ -65,14 +86,14 @@ const api = {
       })
 
       return pensamentosFiltrados;
-    } catch{
+    } catch {
       alert("Erro ao filtrar pensamento");
     }
   },
 
   async atualizarFavorito(id, favorito) {
-    try{
-      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, {favorito})
+    try {
+      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
       return response.data
     } catch (error) {
       alert("Erro ao atualizar favorito")
--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
+const pensamentosSet = new Set();
+
+async function adicionarChaveAoPensamento(){
+  try{
+    const pensamentos = await api.buscarPensamentos();
+    pensamentos.forEach(pensamento => {
+      const chave = `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`;
+      pensamentosSet.add(chave);
+    })
+  }catch(error){
+    alert("Erro ao adicionar chave ao pensamento");
+  }
+}
+
+function removerEspaços(string){
+  return string.replaceAll(/\s+/g, '');
+}
+
+const regexConteudo = /^[A-Za-z\s]{10,}$/;
+const regexAutoria = /^[a-zA-Z]{3,15}$/;
+
+function validarConteudo(conteudo){
+  return regexConteudo.test(conteudo);
+}
+
+function validarAutoria(autoria){
+  return regexAutoria.test(autoria);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos()
+  adicionarChaveAoPensamento()
 
   const formularioPensamento = document.getElementById("pensamento-form")
   const botaoCancelar = document.getElementById("botao-cancelar")
-  const inputBusca = document.getElementById("campo-busca");
+  const inputBusca = document.getElementById("campo-busca")
 
-  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
-  botaoCancelar.addEventListener("click", manipularCancelamento);
-  inputBusca.addEventListener("input", manipularBusca);
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
+  inputBusca.addEventListener("input", manipularBusca)
 })
 
 async function manipularSubmissaoFormulario(event) {
@@ -20,8 +50,30 @@ async function manipularSubmissaoFormulario(event) {
   const autoria = document.getElementById("pensamento-autoria").value
   const data = document.getElementById("pensamento-data").value
 
-  if(!validarData(data)){
-    alert("Não é permitada o cadastro de datas futuras, selecione uma data válida");
+  const conteudoSemEspaços = removerEspaços(conteudo)
+  const autoriaSemEspaços = removerEspaços(autoria)
+
+  if (!validarConteudo(conteudoSemEspaços)) {
+    alert("É permida a inclusão de letras e espaços com no mínimo 10 caracteres no campo 'conteudo'")
+    return;
+  }
+
+  if (!validarAutoria(autoriaSemEspaços)) {
+    alert("É permitida a inclusão de letras com no mínimo 3 e no máximo 15 caracteres no campo 'autoria ou fonte' ")
+    return;
+  }
+
+  if (!validarData(data)) {
+    alert("Não é permitido o cadastro de datas futuras. Selecione outra data")
+    return
+  }
+
+  const chaveNovoPensamento = 
+  `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`;
+
+  if(pensamentosSet.has(chaveNovoPensamento)){
+    alert("Pensamento já cadastrado")
+    return;
   }
 
   try {
@@ -42,17 +94,16 @@ function manipularCancelamento() {
 
 async function manipularBusca() {
   const termoBusca = document.getElementById("campo-busca").value
-  try{
-    const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoBusca);
+  try {
+    const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoBusca)
     ui.renderizarPensamentos(pensamentosFiltrados)
-  } catch(error){
-    alert("Erro ao realizar busca");
+  } catch (error) {
+    alert("Erro ao realizar busca")
   }
 }
 
 function validarData(data) {
-  const dataAtual = new Date();
-  const dataInserida = new Date(data);
-
-  return dataInserida <= dataAtual;
+  const dataAtual = new Date()
+  const dataInserida = new Date(data)
+  return dataInserida <= dataAtual
 }
